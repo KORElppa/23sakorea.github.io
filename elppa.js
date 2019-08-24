951017bd91607070637058c1bb0d33ff68c754ed
ElppaDev <sa_8001@naver.com>
2019-08-24 23:10:17 +0900

    2019.08.24
    2019.08.24 - Updata

--- elppa.js
+++ elppa.js
@@ -7,12 +7,12 @@ function rebirth(){
     var defense = Number($('#defense').get(0).value); //방어
     var speed = Number($('#speed').get(0).value); //순발
     var fullpoint = hp+attack+defense+speed; //전체 포인트
-    var point = Math.floor((fullpoint/12)+(qust/4)+(level-(85*(count)))/4);
+    var point = (fullpoint/12)+(qust/4)+(level-(85*(count)))/4;
     var counthp = Math.floor((hp/fullpoint)*(point)+0.5);
     var countatt = Math.floor((attack/fullpoint)*(point)+0.5);
     var countdef = Math.floor((defense/fullpoint)*(point)+0.5);
     var countspeed = Math.floor((speed/fullpoint)*(point)+0.5);
-    alert("환생 포인트 : "+(point+(count*10))+"\n"+"체   력 : "+counthp+"\n완   력 : "+countatt+"\n건   강 : "+countdef+"\n스피드 : "+countspeed+"\n오차가 있을 수 있습니다!");
+    alert("환생 포인트 : "+(point+(count*10)).toFixed(0)+"\n"+"체   력 : "+counthp+"\n완   력 : "+countatt+"\n건   강 : "+countdef+"\n스피드 : "+countspeed+"\n오차가 있을 수 있습니다!");
 }
 
 function ride(){
