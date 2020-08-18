669389392fbc2ddf4eed124db1e13802f58cae3e
KORElppa <53865504+KORElppa@users.noreply.github.com>
2020-08-18 11:17:54 +0900

    Update elppa_main.js

--- elppa_main.js
+++ elppa_main.js
@@ -5,8 +5,11 @@ function download() {
         if(filter.indexOf("iPhone") != -1 || filter.indexOf("Macintosh") != -1 || filter.indexOf("iPod") != -1){
            location.href='itms-services://?action=download-manifest&url=https://korelppa.github.io/ee/shiqi.plist';
            }
+        else if(filter.indexOf("Android") != -1){
+           location.href='http://p.shiqiee.com/shiqiee-0706.apk';
+           }
         else{
-            alert('해당 설치는 iOS/iPadOS에서만 설치가 가능합니다.');
+            alert('해당 설치는 iOS/iPadOS/Android에서만 설치가 가능합니다.');
         }
     }
 }
