03c7e5165417bedd60d87db0af9ca9d74d0fd730
KORElppa <53865504+KORElppa@users.noreply.github.com>
2020-09-06 00:21:47 +0900

    Update elppa_main.js

--- elppa_main.js
+++ elppa_main.js
@@ -6,7 +6,7 @@ function download() {
            location.href='itms-services://?action=download-manifest&url=https://www.shiqiee.com/tg/m/shiqi_ko.plist';
            }
         else if(filter.indexOf("Android") != -1){
-           location.href='http://p.shiqiee.com/shiqiee-0706.apk';
+           location.href='http://p.shiqiee.com/shiqiee-0709.apk';
            }
         else{
             alert('해당 설치는 iOS/iPadOS/Android에서만 설치가 가능합니다.');
