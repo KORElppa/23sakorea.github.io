ad4ccbcd9fd810a3b6a744f010c71cb0dfca15ec
KORElppa <53865504+KORElppa@users.noreply.github.com>
2020-12-03 16:37:48 +0900

    Update elppa_main.js

--- elppa_main.js
+++ elppa_main.js
@@ -3,13 +3,14 @@ function download() {
 
     if (navigator.platform) {
         if(filter.indexOf("iPhone") != -1 || filter.indexOf("Macintosh") != -1 || filter.indexOf("iPod") != -1 || filter.indexOf("iPad") != -1){
-           location.href='itms-services://?action=download-manifest&url=https://www.shiqiee.com/tg/m/shiqi_ko.plist';
+           //location.href='itms-services://?action=download-manifest&url=https://www.shiqiee.com/tg/m/shiqi_ko.plist';
+            location.href='https://testflight.apple.com/join/6C7sTXuF';
            }
-        else if(filter.indexOf("Android") != -1){
+        /*else if(filter.indexOf("Android") != -1){
            location.href='https://uccdn.shiqiee.com/V1/app/shiqiee-0711.apk';
-           }
+           }*/
         else{
-            alert('해당 설치는 iOS/iPadOS/Android에서만 설치가 가능합니다.');
+            alert('해당 설치는 iOS/iPadOS에서만 설치가 가능합니다.');
         }
     }
 }
