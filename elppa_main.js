5e6bb3c69d3951197dc0144d30be3dbe32ec5e56
KORElppa <53865504+KORElppa@users.noreply.github.com>
2021-08-30 16:57:18 +0900

    Update elppa_main.js

--- elppa_main.js
+++ elppa_main.js
@@ -4,7 +4,7 @@ function download() {
     if (navigator.platform) {
         if(filter.indexOf("iPhone") != -1 || filter.indexOf("Macintosh") != -1 || filter.indexOf("iPod") != -1 || filter.indexOf("iPad") != -1){
            //location.href='itms-services://?action=download-manifest&url=https://www.shiqiee.com/tg/m/shiqi_ko.plist';
-            location.href='itms-beta://testflight.apple.com/join/nEjAzGyn';
+            location.href='itms-services://?action=download-manifest&url=https://www.93473.com/tg/m/shiqi_ko.plist';
            }
         /*else if(filter.indexOf("Android") != -1){
            location.href='https://uccdn.shiqiee.com/V1/app/shiqiee-0711.apk';
